ebe94446ccba9876fa44aeae3ebd5fabeeab9f34
Alina Kozlyuk <talina787@icloud.com>
2026-03-03 12:04:43 +0200

    task2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,47 @@
 const formData = { email: "", message: "" };
+
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('.js-email');
+const message = form.querySelector('.js-message');
+
+form.addEventListener('input', e => {
+    const { name, value } = e.target;
+    
+    formData[name] = value.trim();
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+})
+
+
+const storage = localStorage.getItem("feedback-form-state");
+
+if (storage) {
+    const parsedData = JSON.parse(storage);
+
+    formData.email = parsedData.email;
+    formData.message = parsedData.message;
+
+    email.value = parsedData.email;
+    message.value = parsedData.message;
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if (formData.email.trim() === '' || formData.message.trim() === '') {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log(formData);
+
+    localStorage.removeItem("feedback-form-state");
+
+    form.reset();
+
+    formData.email = '';
+    formData.message = '';
+    
+})
+
+
